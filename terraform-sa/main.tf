27cad8504e89accea38e1606cd4182aaecd000cc
OutOfDevOps <anto@morningwall.com>
2021-09-30 20:33:37 +0100

    Adding workflow

--- terraform-sa/main.tf
+++ terraform-sa/main.tf
@@ -4,12 +4,16 @@ resource "google_service_account" "runner_sa" {
   display_name = "Service Account"
 }
 
+data "google_project" "project" {
+  project_id = var.project_id
+}
+
 data "google_iam_policy" "wli_user_ghshr" {
   binding {
     role = "roles/iam.workloadIdentityUser"
 
     members = [
-      "principalSet://iam.googleapis.com/projects/950564871957/locations/global/workloadIdentityPools/gh-pool/attribute.full/outofdevops/ghshr-gcp-wlifrefs/heads/main",
+      "principalSet://iam.googleapis.com/projects/${data.google_project.project.number}/locations/global/workloadIdentityPools/gh-pool/attribute.full/${var.gh_repo}${var.gh_branch}",
     ]
   }
 }
--- terraform-sa/vars.tf
+++ terraform-sa/vars.tf
@@ -2,3 +2,13 @@ variable "project_id" {
   type = string
   description = "The Google Project ID"
 }
+
+variable "gh_repo" {
+  type = string
+  description = "The GitHub repo in the format username/repo_name"
+}
+
+variable "gh_branch" {
+  type = string
+  description = "The Branch on which the Workflow execution will be authorised in the format refs/heads/<BRANCH_NAME>"
+}
